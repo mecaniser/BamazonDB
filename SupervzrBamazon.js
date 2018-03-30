404c86dec2fc8c6398b998b8a61aa072e082f51a
mecaniser <35816502+mecaniser@users.noreply.github.com>
2018-03-29 23:32:58 -0400

    Client  Node application, will allow to update, modify and manage overall Database structure.

---
+++ SupervzrBamazon.js
@@ -0,0 +1,13 @@
+
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+
+	user: 'root',
+
+	password: '',
+	database: 'BamazonDB'
+});
