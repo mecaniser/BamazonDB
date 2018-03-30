e52d49d3b52b0ced81929acad9de5f74264860b0
mecaniser <35816502+mecaniser@users.noreply.github.com>
2018-03-29 23:38:46 -0400

    MySQL Database, initial setup

---
+++ BamazonDB.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS BamazonDB;
+CREATE DATABASE BamazonDB;
+USE BamazonDB;
+
+CREATE TABLE products (
+	item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+	product_name VARCHAR(30) NOT NULL,
+	department_name VARCHAR(20) NOT NULL,
+	price DECIMAL(10,2) NOT NULL,
+	stock_quantity INTEGER(11) NOT NULL,
+	PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES  ();
